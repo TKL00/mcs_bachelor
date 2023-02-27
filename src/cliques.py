965d717711cf8bfd9dae477479600c3dce02fb8e
toleh20 <toleh20@student.sdu.dk>
2023-02-27 09:21:18 +0100

    Empty cliques.py

--- src/cliques.py
+++ src/cliques.py
@@ -1,6 +1,11 @@
-
-def line_graph(G):
-    return None
-
+### Authors: Tobias Klink Lehn (toleh20@student.sdu.dk) and Kasper Halkjær Beider (kbeid20@student.sdu.dk)
 def modular_product(G):
     return None
+
+## G and H to their respective Line Graphs
+
+## Modular Product of G and H
+
+## Find maximum Clique that contains the anchor point
+
+## Convert from Line Graph to 
