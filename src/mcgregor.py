9f85b03adf7328a4c1dd2394b8a72a5b87cc2eaa
toleh20 <toleh20@student.sdu.dk>
2023-02-27 09:21:03 +0100

    Fixed McGregor to only map nodes where edge correspondance is valid

--- src/mcgregor.py
+++ src/mcgregor.py
@@ -1,10 +1,12 @@
-### Authors: Tobias Klink Lehn (toleh20@student.sdu.dk) and Kasper Halkjær Beider (kbeid20@student.sdu.dk)
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 from copy import deepcopy
 from workspace import Workspace
+from linegraph import line_graph as lg
+from linegraph import convert_edge_anchor
 
+### Authors: Tobias Klink Lehn (toleh20@student.sdu.dk) and Kasper Halkjær Beider (kbeid20@student.sdu.dk)
 def mcs_mcgregor(G, H, anchor_point={}):
     """
     Computes the Maximum Common Subgraph using the Algorithm suggested by
@@ -68,6 +70,30 @@ def mcs_mcgregor(G, H, anchor_point={}):
                                 if MARCS_row_ones[G_edge] == 0:
                                     arcsleft -= 1
         return arcsleft
+    
+    def is_legal_pair(g_node, h_node, G, H, mapping):
+        """
+            Determines whether the g_node in G can be successfully mapped to the h_node in H. g_node can be mapped to h_node
+            if and only if edges from g_node in the current mapping correlates to edges from h_node in the current mapping.
+            That is, if h_node has an edge between itself and a node in the current subgraph of H but this edge doesn't exist between
+            g_node and that corresponding node in the subgraph G, g_node cannot be mapped to h_node.
+        """
+        g_neighbours = G.adj[g_node]
+        h_neighbours = H.adj[h_node]
+
+        h_allowed_neighbours = []
+        for neighbour in g_neighbours:
+            ## only account for nodes in G that have been mapped to nodes in H
+            if mapping[neighbour] != "":
+                h_allowed_neighbours.append(mapping[neighbour])
+
+        for neighbour in h_neighbours:
+            ## If the h_node's neighbour is a part of the current MCS in H
+            ## but this neighbour does not exist in G, a contradiction has been found.
+            if neighbour in mapping.values() and neighbour not in h_allowed_neighbours:
+                return False
+        
+        return True 
 
     ## Initialization
     G_node_amt, H_node_amt = len(G.nodes), len(H.nodes)
@@ -97,7 +123,7 @@ def mcs_mcgregor(G, H, anchor_point={}):
     ## Array to save workspaces containing MARCS, arcsleft, MARCS_row_ones , and killed_edges
     workspaces = [0 for i in range(G_node_amt)]
 
-    ## The current G -> H function as well as a list of all mappings, MARCSs and arcs_left
+    ## The current G -> H function, MARCSs and arcs_left
     current_mapping = {}
     best_mapping = [""]
     for node_index in range(G_node_amt):
@@ -135,16 +161,19 @@ def mcs_mcgregor(G, H, anchor_point={}):
         v += 1
 
     ## In case all nodes in G are anchored, the algorithm needs not to run.
-    if v >= G_node_amt: return current_mapping, MARCS, arcsleft
+    if v >= G_node_amt:
+        return current_mapping, MARCS, arcsleft
     first_non_anchor = v
     
-    ##                                              ALGORITHM BEGINS HERE
+    #####################################################################################################################
+    ##                                              ALGORITHM BEGINS HERE                                              ##
+    #####################################################################################################################
     while v >= 0: 
         x = None
         ## Finding a node x in H that has not already been mapped to.  
         ## Additionally, that node x in H has not been tried yet by node v in G
         for H_node in range(H_node_amt):
-            if not H_tried[v][H_node] and not H_mapped[H_node]:
+            if not H_tried[v][H_node] and not H_mapped[H_node] and is_legal_pair(v, H_node, G, H, current_mapping):
                 x = H_node
                 ## If v is currently mapped to a different node
                 ## update said node to no longer be mapped.
@@ -173,13 +202,13 @@ def mcs_mcgregor(G, H, anchor_point={}):
             x_edges = H_node_to_arc[x]
             
             arcsleft = update_MARCS(MARCS, v_edges, x_edges, killed_edges, MARCS_row_ones, arcsleft)
-                                        
-            ## 
+
+            ## If the number of edges to be mapped is 'high', we either build further down the branch
+            ## or save the current mapping if in a leaf node.
             if arcsleft > bestarcsleft:
                 if v == G_node_amt - 1:
                     best_mapping[0] = (deepcopy(current_mapping), deepcopy(MARCS), arcsleft)
                     counter += 1
-                    
                     bestarcsleft = arcsleft
                 else:
                     ## Store values in workspace associated with node v
@@ -231,7 +260,21 @@ def pretty_printer_mapping(mapping):
     for v in mapping:
         print(v, "->", mapping[v])
 
-def pretty_printer_graphs(G, H, mapping, marcs, anchor):
+def pretty_printer_graphs(G, H, mapping, marcs, anchor={}):
+    """
+    Provides a graphical representation of graphs G and H, highlighting
+    their subgraph denoted by the 'mapping', 'marcs' and an optional anchor mapping.
+
+        `Parameters`:
+            G (Graph): A NetworkX graph, nodes are integers but may be decorated with items
+            H (Graph): A NetworkX graph, nodes are integers but may be decorated with items
+            mapping (dict: int -> int): The node correspondence for the MCS
+            marcs (np.array): The MARCS array for the MCS
+
+        `Optional`:
+            anchor (dict: int -> int): The initial anchor point. Can be declared with dict([(a, b), (b, c), ...])
+    """
+    
     
     ## String used for nodes mapped to each other
     label_string="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -262,7 +305,7 @@ def pretty_printer_graphs(G, H, mapping, marcs, anchor):
 
     ## G DRAWING
     subax1 = plt.subplot(121)
-    nx.draw_networkx_nodes(G, G_pos, nodelist=G.nodes, node_color=G_node_colors, **options)
+    nx.draw_networkx_nodes(G, G_pos, nodelist=sorted(G.nodes), node_color=G_node_colors, **options)
     nx.draw_networkx_edges(G, G_pos, width=1.0, alpha=0.5)
     
     ## Determine which edges exist in the mapping and color those.
@@ -314,7 +357,7 @@ def pretty_printer_graphs(G, H, mapping, marcs, anchor):
 
     ## H DRAWING
     subax2 = plt.subplot(122)
-    nx.draw_networkx_nodes(H, nodelist=H.nodes, node_color=H_node_colors, pos=H_pos)
+    nx.draw_networkx_nodes(H, nodelist=sorted(H.nodes), node_color=H_node_colors, pos=H_pos)
     nx.draw_networkx_edges(H, H_pos, width=1.0, alpha=0.5)
     nx.draw_networkx_edges(
         H,
@@ -328,20 +371,137 @@ def pretty_printer_graphs(G, H, mapping, marcs, anchor):
 
     plt.show()
 
+def pretty_printer_line_graphs(G, H, LG, LH, mapping, marcs, edge_anchor={}, line_node_anchor={}):
+
+    """
+        Mapping is the node mapping from nodes in LG to nodes in LH.
+    """
+
+    label_string="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    G_edge_labels, H_edge_labels, G_node_labels, H_node_labels, LG_node_labels, LH_node_labels = {}, {}, {}, {}, {}, {}
+
+    G_edges = list(G.edges)
+    H_edges = list(H.edges)
+    LG_edges = list(LG.edges)
+    LH_edges = list(LH.edges)
+
+    G_pos = nx.spring_layout(G, seed=1010203)
+    H_pos = nx.spring_layout(H, seed=1010203)
+    LG_pos = nx.spring_layout(LG, seed=1010203)
+    LH_pos = nx.spring_layout(LH, seed=1010203)
+
+    ## Node colours
+    H_node_colors = ["tab:pink" for i in range(len(H.nodes))]
+    G_node_colors = ["tab:blue" for i in range(len(G.nodes))]
+    LH_node_colors = ["tab:pink" for i in range(len(LH.nodes))]
+    LG_node_colors = ["tab:blue" for i in range(len(LG.nodes))]
+
+    # Edge colors in line graph mapping
+    LG_color_edges = []
+    LH_color_edges = []
+    for i in range(len(marcs)):
+        for j in range(len(marcs[0])):
+            if marcs[i][j] == 1:
+                LG_color_edges.append(LG_edges[i])
+                LH_color_edges.append(LH_edges[j])
+
+    ## edge color for anchored edges
+    G_anchor_color_edges = [keys for keys in edge_anchor]
+    H_anchor_color_edges = [edge_anchor[keys] for keys in edge_anchor]
+
+    ## node color for anchored nodes in line graphs
+    LG_anchor_color_nodes = [keys for keys in line_node_anchor]
+    LH_anchor_color_nodes = [line_node_anchor[keys] for keys in line_node_anchor]
+
+    ## 1-n numbering of the nodes
+    for i in range(len(G.nodes)):
+        G_node_labels[i] = i
+    
+    for i in range(len(H.nodes)):
+        H_node_labels[i] = i
+
+    for i in range(len(LG.nodes)):
+        LG_node_labels[i] = i
+    
+    for i in range(len(LH.nodes)):
+        LH_node_labels[i] = i
+
+    ## Compute labels for the edges and nodes in the mapping
+    for LG_node in mapping:
+        LH_node = mapping[LG_node]
+
+        LG_node_labels[LG_node] = label_string[LG_node]
+        LH_node_labels[LH_node] = label_string[LG_node]
+        
+        G_edge_labels[G_edges[LG_node]] = label_string[LG_node]
+        H_edge_labels[H_edges[LH_node]] = label_string[LG_node]
+    
+    ###                 DRAWING
+
+    ## G
+    subax1 = plt.subplot(221)
+    subax1.set_title("G")
+    nx.draw_networkx_nodes(G, G_pos, nodelist=sorted(G.nodes), node_color=G_node_colors)
+    nx.draw_networkx_edges(G, G_pos, width=1.0, alpha=0.5)
+    ## color anchored edges
+    nx.draw_networkx_edges(G, G_pos, width=8, alpha=0.5, edgelist=G_anchor_color_edges, edge_color="tab:orange")
+    nx.draw_networkx_labels(G, G_pos, G_node_labels, font_size=15, font_color="whitesmoke")
+    nx.draw_networkx_edge_labels(G, G_pos, G_edge_labels)
+
+    ## H
+    subax2 = plt.subplot(222)
+    subax2.set_title("H")
+    nx.draw_networkx_nodes(H, H_pos, nodelist=sorted(H.nodes), node_color=H_node_colors)
+    nx.draw_networkx_edges(H, H_pos, width=1.0, alpha=0.5)
+    ## color anchored edges
+    nx.draw_networkx_edges(H, H_pos, width=8, alpha=0.5, edgelist=H_anchor_color_edges, edge_color="tab:orange")
+    nx.draw_networkx_labels(H, H_pos, H_node_labels, font_size=15, font_color="whitesmoke")
+    nx.draw_networkx_edge_labels(H, H_pos, H_edge_labels)
+
+    ## LG
+    subax3 = plt.subplot(223)
+    subax3.set_title("Linegraph of G")
+    nx.draw_networkx_nodes(LG, LG_pos, nodelist=sorted(LG.nodes), node_color=LG_node_colors)
+    nx.draw_networkx_nodes(LG, LG_pos, nodelist=LG_anchor_color_nodes, node_color="tab:orange")
+    nx.draw_networkx_edges(LG, LG_pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_edges(LG, LG_pos, edgelist=LG_color_edges, width=5, alpha=0.5, edge_color="tab:red")
+    nx.draw_networkx_labels(LG, LG_pos, LG_node_labels, font_size=15, font_color="whitesmoke")
+
+    ## LH
+    subax4 = plt.subplot(224)
+    subax4.set_title("Linegraph of H")
+    nx.draw_networkx_nodes(LH, LH_pos, nodelist=sorted(LH.nodes), node_color=LH_node_colors)
+    nx.draw_networkx_nodes(LH, LH_pos, nodelist=LH_anchor_color_nodes, node_color="tab:orange")
+    nx.draw_networkx_edges(LH, LH_pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_edges(LH, LH_pos, edgelist=LH_color_edges, width=5, alpha=0.5, edge_color="tab:red")
+    nx.draw_networkx_labels(LH, LH_pos, LH_node_labels, font_size=15, font_color="whitesmoke")
+    
+
+    plt.show()
+
 if __name__ == "__main__":
 
     G = nx.Graph()
-    G.add_edges_from([(0,1), (1,2), (2,0), (2,3)])
-    anchor = dict([(0,10)])
+    G.add_edges_from([(0,1), (0,2), (1,2), (2,3), (3,4)])
 
-    H = nx.complete_graph(10)
-    H.add_edge(9,10)
-    # H = nx.graph_atlas_g()[120]
+    H = nx.Graph()
+    H.add_edges_from([(0,1), (1,2), (1,3), (1,7), (2,3), (3,4), (3,6), (6, 7), (7, 8), (4,5)])
     print("G EDGES: ", G.edges)
     print("H EDGES:", H.edges)
 
-    mapping, marcs, arcsleft = mcs_mcgregor(G, H, anchor)
+    edge_anchor = {
+                    (0, 1): (7, 8),
+                    (0, 2): (7, 6)
+                    }
+    
+    line_node_anchor = convert_edge_anchor(G, H, edge_anchor)
+
+    LG = lg(G)
+    LH = lg(H)
+
+    mapping, marcs, arcsleft = mcs_mcgregor(LG,LH, line_node_anchor)
     pretty_printer_mapping(mapping)
 
     ## DRAWING
-    pretty_printer_graphs(G, H, mapping, marcs, anchor)
+    # pretty_printer_graphs(LG,LH, mapping, marcs, anchor=line_node_anchor)
+    pretty_printer_line_graphs(G, H, LG, LH, mapping, marcs, edge_anchor=edge_anchor, line_node_anchor=line_node_anchor)
