5fbce51ef63e708d9be2a5328cd7cc8fa7098b19
toleh20 <toleh20@student.sdu.dk>
2023-03-09 13:58:30 +0100

    Added colors to distinguish edge types in product graph

--- src/productgraph.py
+++ src/productgraph.py
@@ -8,13 +8,16 @@ def product_graph(G, H):
     """
 
     product_graph = nx.Graph()
-    product_nodes = itertools.product(G.nodes, H.nodes)
+
+    ## Computes the cartesian products of the two node sets (sorted)
+    product_nodes = itertools.product(sorted(G.nodes), sorted(H.nodes))
     product_graph.add_nodes_from(product_nodes)
     node_count = len(product_graph.nodes)
 
     product_graph_nodes = list(product_graph.nodes)
 
     for i in range(node_count):
+        ## "u" and "x" from G, "v" and "y" from H
         (u, v) = product_graph_nodes[i]
         for j in range(i + 1, node_count):
             (x, y) = product_graph_nodes[j]
@@ -23,9 +26,9 @@ def product_graph(G, H):
                 u_neighbours, v_neighbours = list(G.adj[u]), list(H.adj[v])
 
                 if x in u_neighbours and y in v_neighbours:
-                    product_graph.add_edge( (u, v), (x, y) )
+                    product_graph.add_edge( (u, v), (x, y), color="blue" )
                 
                 elif x not in u_neighbours and y not in v_neighbours:
-                    product_graph.add_edge( (u, v), (x, y) )
+                    product_graph.add_edge( (u, v), (x, y), color="red" )
 
     return product_graph 
