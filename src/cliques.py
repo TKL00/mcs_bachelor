44bafe96f74250a7b4c45ecc98f52c2127dba81b
toleh20 <toleh20@student.sdu.dk>
2023-03-14 10:57:10 +0100

    Implemented Akbar's Algorithm 1 for blue connected components

--- src/cliques.py
+++ src/cliques.py
@@ -2,8 +2,10 @@ from productgraph import product_graph as pg
 from linegraph import line_graph as lg
 from linegraph import convert_edge_anchor_lg
 import networkx as nx
-from draw_graphs import draw_product_graph
-from draw_graphs import draw_two_graphs
+from queue import Queue
+import copy
+from draw_graphs import draw_product_graph, draw_two_graphs, draw_blue_connected_components
+
 
 ### Authors: Tobias Klink Lehn (toleh20@student.sdu.dk) and Kasper Halkjær Beider (kbeid20@student.sdu.dk)
 def mcs_leviBarrowBurstall(G, H, node_anchor={}, edge_anchor={}):
@@ -29,6 +31,135 @@ def mcs_leviBarrowBurstall(G, H, node_anchor={}, edge_anchor={}):
             
     """
 
+    def blue_component_filter(PG, N, A, edge_color_lookup):
+        """
+            Given the "blue" neighbourhood N of the anchored nodes in A in the Product Graph PG, computes a list of
+            disjoint blue connected components reachable from the anchor point via blue edges. Blue connected components
+            not reachable from A via blue edges are dismissed.
+
+            Precondition: All nodes in A are connected to all nodes in N either by blue or red edges.
+
+            `Parameters`:
+
+                PG (Graph): A NetworkX graph, nodes are tuples of integers (u, v) but may be decorated with items
+                
+                N (list(node)): A list of nodes in the blue neighbourhood of A. That is, if a node is in N, it is adjacent to all nodes in A.
+
+                A (list(node)): A list of anchored nodes in PG.
+
+                edge_color_lookup (dict(edge -> str)): A dictionary mapping edges in PG to a color. The value may either be "red" or "blue"
+            
+            `Returns`:
+
+                all_components (list( list(node) )): A list of blue connected components all reachable by blue edges from the anchor, A.
+        """
+        ## BFS to find all nodes that are connected to A through blue edges
+        ## all nodes in the product graph are white in the beginning
+        ## NOTE: optimization for not whitening all the nodes in PG, but only the ones in A and N.
+        
+        ## Avoiding all nodes not in A and N by giving them a scary color
+        node_color_lookup = { node: "p" for node in PG.nodes}
+        for nodes in A:
+            node_color_lookup[nodes] = "w"
+        for nodes in N:
+            node_color_lookup[nodes] = "w"
+        
+        ## Starting from a node in the anchor and running BFS from that, 
+        ## modified to only look at the neighbours that can be reached via a blue edge
+        source = A[0]
+        Q = Queue()
+        Q.put(source)
+        while not Q.empty():
+            u = Q.get()
+            for v in PG.adj[u]:
+                if node_color_lookup[v] == "w":
+                    if u < v:
+                        edge_color = edge_color_lookup[(u, v)]
+                    else:
+                        edge_color = edge_color_lookup[(v, u)]
+                    
+                    ## Don't consider nodes that are reached by red edges
+                    if edge_color == "blue":
+                        node_color_lookup[v] = "g"
+                        Q.put(v)
+            node_color_lookup[u] = "b"
+
+        ## all nodes with a "black" color can be reached from A via blue edges. The reached nodes are either in A or N.
+        marked_nodes = [nodes for nodes in node_color_lookup if node_color_lookup[nodes] == "b"]
+        
+        ## disregard nodes in the neighbourhood not reachable by blue edges
+        filtered_N = [nodes for nodes in N if nodes in marked_nodes]
+        
+        ## Only the anchor
+        if len(filtered_N) == 0:
+            return []
+
+        ## BFS on filtered nodes to find their connected components
+        ## reset color of blue nodes
+        for blue_nodes in filtered_N:
+            node_color_lookup[blue_nodes] = "w"
+
+        all_components = []
+        ## continue BFS until all components are found (i.e. when all nodes of the filtered_N have been re-colored)
+        while len(filtered_N) > 0:
+            blue_source = filtered_N.pop()
+            Q.put(blue_source)
+            ## New component consists of at least this one node from the blue neighbourhood.
+            current_component = [blue_source]        
+            while not Q.empty():
+                u = Q.get()
+                for v in PG.adj[u]:
+                    if node_color_lookup[v] == "w":
+                        if u < v:
+                            edge_color = edge_color_lookup[(u, v)]
+                        else:
+                            edge_color = edge_color_lookup[(v, u)]
+                        
+                        ## Don't consider nodes that are reached by red edges
+                        if edge_color == "blue":
+                            node_color_lookup[v] = "g"
+                            current_component.append(v)
+                            Q.put(v)
+                node_color_lookup[u] = "b"
+            ## remaining blue neighbours who haven't built their own component yet. 
+            filtered_N = [nodes for nodes in filtered_N if node_color_lookup[nodes] == "w"]
+            all_components.append(current_component)
+            current_component = []
+
+        return all_components
+
+    def connected_MCS(listN, PG, A, edge_color_lookup):
+        """
+            Implementation of Algorithm 1 as suggested by Akbar Davoodi.
+
+            Finds all maximal cliques in all blue connected components and creates
+            the union of these with the anchor point.
+        """
+        MCSs = set(copy.deepcopy(A))
+
+        for component in listN:
+            ## create vertex induced subgraph
+            comp_subgraph = nx.Graph(nx.induced_subgraph(PG, component))
+            ## compute L
+            cliques = list(nx.find_cliques(comp_subgraph))
+            print(f"cliques: {cliques}")
+            for clique in cliques:
+                blue_edge_found = False
+                for node in clique:
+                    neighbs = PG.adj[node]
+                    for neighbour in neighbs:
+                        if neighbour in A:
+                            ## determine edge color between the node and its anchor neighbour (if one exists)
+                            edge_color = edge_color_lookup[(neighbour, node)] if neighbour < node else edge_color_lookup[(node, neighbour)]
+                            if edge_color == "blue":
+                                ## Upon blue edge discovery, add entire clique accounting for duplicates
+                                MCSs.update(clique)
+                                blue_edge_found = True
+                                break
+                    ## pursue next clique if this one already has a blue edge
+                    if blue_edge_found: break
+        return MCSs
+
     ## Use Line-Graph implementation
     if edge_anchor:
         LG = lg(G)
@@ -39,17 +170,37 @@ def mcs_leviBarrowBurstall(G, H, node_anchor={}, edge_anchor={}):
         
         ## Compute product graph.
         mod_product_graph = pg(LG, LH)
+        ## Mapping edges in the "LG x LH" to their color
+        color_dictionary = nx.get_edge_attributes(mod_product_graph, "color")
 
         ## Anchor points in product graph correspond to (x, y) nodes where 
         ## x is a node in LG and y is a node in LH.
         anchor_points = [ (g, computed_node_anchor[g]) for g in computed_node_anchor ]
-    
+
+        ## computing N by intersecting all neighbours among the anchor points (NOTE: possible optimization when computing the intersection)
+        common_neighbours_N = list(mod_product_graph.adj[anchor_points[0]].keys())
+        for nodes in anchor_points:
+            common_neighbours_N = [value for value in common_neighbours_N if value in mod_product_graph.adj[nodes].keys()]
+            
+        listN = blue_component_filter(mod_product_graph, common_neighbours_N, anchor_points, color_dictionary)
+
+        ## If no components exist, the anchor is the MCS
+        if len(listN) == 0:
+            return edge_anchor
+        else:
+            MCSs = connected_MCS(listN, mod_product_graph, anchor_points, color_dictionary)
+            print(f"Anchor: {anchor_points}")
+            print(f"Blue components: {listN}")
+            print(f"MCS: {MCSs}")
+            draw_blue_connected_components(mod_product_graph, anchor_points, listN, color_dictionary)
+
     ## No use of line graphs
     else:
         mod_product_graph = pg(G, H)
 
         anchor_points = [ (g, node_anchor[g]) for g in node_anchor]
-    
+
+                                            ### FIND CLIQUES CONTAINING ANCHOR AND RETURN MAP
     ## Finds all cliques containing the anchor points
     anchored_cliques = list(nx.find_cliques(mod_product_graph, anchor_points))
 
@@ -105,16 +256,15 @@ if __name__ == "__main__":
         4: 5
     }
 
-    pgh = pg(G, H)
+    # pgh = pg(G, H)
 
-    print("RESULT FROM EDGE ANCHOR")
     subgraphs = mcs_leviBarrowBurstall(G, H, edge_anchor=edge_anchor)
-    print(subgraphs)
+    # draw_two_graphs(G, H, subgraphs[0])
 
-    print("RESULT FROM NODE ANCHOR")
-    subgraphs = mcs_leviBarrowBurstall(G, H, node_anchor=node_anchor)
-    print(subgraphs)
-    draw_two_graphs(G, H)
+    # print("RESULT FROM NODE ANCHOR")
+    # subgraphs = mcs_leviBarrowBurstall(G, H, node_anchor=node_anchor)
+    # print(subgraphs)
+    # draw_two_graphs(G, H)
     
 
 
