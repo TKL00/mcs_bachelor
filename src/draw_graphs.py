99efbe10a6bf716650b4a958aca155f05ab054c1
toleh20 <toleh20@student.sdu.dk>
2023-05-09 14:42:23 +0200

    Removed redundant functions and corrected import statements

--- src/draw_graphs.py
+++ src/draw_graphs.py
@@ -2,7 +2,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-from linegraph import convert_edge_anchor_lg
 import copy
 
 ### Authors: Tobias Klink Lehn (toleh20@student.sdu.dk) and Kasper Halkjær Beider (kbeid20@student.sdu.dk)
@@ -356,130 +355,6 @@ def draw_mcgregor_mcs_graphs(G, H, mapping, marcs, anchor={}):
         edge_color="tab:blue",
     )
     nx.draw_networkx_labels(H, H_pos, H_node_labels, font_size=10, font_color="whitesmoke")
-
-    plt.show()
-
-
-def draw_mcgregor_mcs_lgs(G, H, LG, LH, mapping, marcs, edge_anchor={}):
-
-    """
-        Provides a graphical representation of graphs G, H and their corresponding linegraphs LG, LH, highlighting
-        their subgraph denoted by the 'mapping', 'marcs' and an optional edge anchor mapping between G and H.
-        The anchored edges between G and H are highlighted, and their corresponding nodes in the line graphs are
-        highlighted accordingly.
-
-        `Parameters`:
-            G (Graph): A NetworkX graph, nodes are integers but may be decorated with items
-            H (Graph): A NetworkX graph, nodes are integers but may be decorated with items
-            LG (Graph): The linegraph of G, nodes are integers but may be decorated with items
-            LH (Graph): The linegraph of H, nodes are integers but may be decorated with items
-            mapping (dict: int -> int): The node correspondence for the MCS in LG and LH.
-            marcs (np.array): The MARCS array for the MCS
-
-        `Optional`:
-            edge_anchor (dict: (int, int) -> (int, int)): The initial anchored edges between G and H.
-    """
-
-    label_string="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    line_node_anchor = convert_edge_anchor_lg(G, H, edge_anchor)
-    G_edge_labels, H_edge_labels, G_node_labels, H_node_labels, LG_node_labels, LH_node_labels = {}, {}, {}, {}, {}, {}
-
-    G_edges = list(G.edges)
-    H_edges = list(H.edges)
-    LG_edges = list(LG.edges)
-    LH_edges = list(LH.edges)
-
-    G_pos = nx.spring_layout(G, seed=GLOBAL_SEED)
-    H_pos = nx.spring_layout(H, seed=GLOBAL_SEED)
-    LG_pos = nx.spring_layout(LG, seed=GLOBAL_SEED)
-    LH_pos = nx.spring_layout(LH, seed=GLOBAL_SEED)
-
-    ## Node colours
-    H_node_colors = ["tab:pink" for i in range(len(H.nodes))]
-    G_node_colors = ["tab:blue" for i in range(len(G.nodes))]
-    LH_node_colors = ["tab:pink" for i in range(len(LH.nodes))]
-    LG_node_colors = ["tab:blue" for i in range(len(LG.nodes))]
-
-    # Edge colors in line graph mapping
-    LG_color_edges = []
-    LH_color_edges = []
-    for i in range(len(marcs)):
-        for j in range(len(marcs[0])):
-            if marcs[i][j] == 1:
-                LG_color_edges.append(LG_edges[i])
-                LH_color_edges.append(LH_edges[j])
-
-    ## edge color for anchored edges
-    G_anchor_color_edges = [keys for keys in edge_anchor]
-    H_anchor_color_edges = [edge_anchor[keys] for keys in edge_anchor]
-
-    ## node color for anchored nodes in line graphs
-    LG_anchor_color_nodes = [keys for keys in line_node_anchor]
-    LH_anchor_color_nodes = [line_node_anchor[keys] for keys in line_node_anchor]
-
-    ## 1-n numbering of the nodes
-    for i in range(len(G.nodes)):
-        G_node_labels[i] = i
-    
-    for i in range(len(H.nodes)):
-        H_node_labels[i] = i
-
-    for i in range(len(LG.nodes)):
-        LG_node_labels[i] = i
-    
-    for i in range(len(LH.nodes)):
-        LH_node_labels[i] = i
-
-    ## Compute labels for the edges and nodes in the mapping
-    for LG_node in mapping:
-        LH_node = mapping[LG_node]
-
-        LG_node_labels[LG_node] = label_string[LG_node]
-        LH_node_labels[LH_node] = label_string[LG_node]
-        
-        G_edge_labels[G_edges[LG_node]] = label_string[LG_node]
-        H_edge_labels[H_edges[LH_node]] = label_string[LG_node]
-    
-    ###                 DRAWING
-
-    ## G
-    subax1 = plt.subplot(221)
-    subax1.set_title("G")
-    nx.draw_networkx_nodes(G, G_pos, nodelist=sorted(G.nodes), node_color=G_node_colors)
-    nx.draw_networkx_edges(G, G_pos, width=1.0, alpha=0.5)
-    ## color anchored edges
-    nx.draw_networkx_edges(G, G_pos, width=8, alpha=0.5, edgelist=G_anchor_color_edges, edge_color="tab:orange")
-    nx.draw_networkx_labels(G, G_pos, G_node_labels, font_size=15, font_color="whitesmoke")
-    nx.draw_networkx_edge_labels(G, G_pos, G_edge_labels)
-
-    ## H
-    subax2 = plt.subplot(222)
-    subax2.set_title("H")
-    nx.draw_networkx_nodes(H, H_pos, nodelist=sorted(H.nodes), node_color=H_node_colors)
-    nx.draw_networkx_edges(H, H_pos, width=1.0, alpha=0.5)
-    ## color anchored edges
-    nx.draw_networkx_edges(H, H_pos, width=8, alpha=0.5, edgelist=H_anchor_color_edges, edge_color="tab:orange")
-    nx.draw_networkx_labels(H, H_pos, H_node_labels, font_size=15, font_color="whitesmoke")
-    nx.draw_networkx_edge_labels(H, H_pos, H_edge_labels)
-
-    ## LG
-    subax3 = plt.subplot(223)
-    subax3.set_title("Linegraph of G")
-    nx.draw_networkx_nodes(LG, LG_pos, nodelist=sorted(LG.nodes), node_color=LG_node_colors)
-    nx.draw_networkx_nodes(LG, LG_pos, nodelist=LG_anchor_color_nodes, node_color="tab:orange")
-    nx.draw_networkx_edges(LG, LG_pos, width=1.0, alpha=0.5)
-    nx.draw_networkx_edges(LG, LG_pos, edgelist=LG_color_edges, width=5, alpha=0.5, edge_color="tab:red")
-    nx.draw_networkx_labels(LG, LG_pos, LG_node_labels, font_size=15, font_color="whitesmoke")
-
-    ## LH
-    subax4 = plt.subplot(224)
-    subax4.set_title("Linegraph of H")
-    nx.draw_networkx_nodes(LH, LH_pos, nodelist=sorted(LH.nodes), node_color=LH_node_colors)
-    nx.draw_networkx_nodes(LH, LH_pos, nodelist=LH_anchor_color_nodes, node_color="tab:orange")
-    nx.draw_networkx_edges(LH, LH_pos, width=1.0, alpha=0.5)
-    nx.draw_networkx_edges(LH, LH_pos, edgelist=LH_color_edges, width=5, alpha=0.5, edge_color="tab:red")
-    nx.draw_networkx_labels(LH, LH_pos, LH_node_labels, font_size=15, font_color="whitesmoke")
-    
 
     plt.show()
 
